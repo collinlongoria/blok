63e0beb388fab4fa67d0e1c8feb6743a03a76674
Collin Longoria <collin.longoria@digipen.edu>
2025-09-04 11:24:11 -0700

    Added Initial CMake and Main File

---
+++ blok/src/main.cpp
@@ -0,0 +1,13 @@
+/*
+* File: main
+* Project: blok
+* Author: Collin Longoria
+* Created on: 9/4/2025
+*/
+
+#include <iostream>
+
+int main(void){
+    std::cout << "blok." << std::endl;
+    return 0;
+}
