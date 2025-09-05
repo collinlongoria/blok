b08f4286b558ff985951687cde000515f1532903
Collin Longoria <collin.longoria@digipen.edu>
2025-09-05 14:02:26 -0700

    Added pre-compiled Dawn to project

--- blok/src/main.cpp
+++ blok/src/main.cpp
@@ -5,12 +5,17 @@
 * Created on: 9/4/2025
 */
 
-#include <webgpu/webgpu.h>
+#include <webgpu.h>
 #include <iostream>
 
 int main(void){
     // Create WebGPU instance
     // Create a descriptor
+    WGPUInstanceDescriptor desc = {};
+    WGPUInstance instance = wgpuCreateInstance(&desc);
+    if(!instance) {
+        std::cerr << "Failed to create WGPU instance" << std::endl;
+    }
 
     std::cout << "blok." << std::endl;
     return 0;
