6923d1b13122706128500f119657533a839e62da
Collin Longoria <collin.longoria@digipen.edu>
2025-09-05 11:28:53 -0700

    fixing dawn

--- blok/src/main.cpp
+++ blok/src/main.cpp
@@ -5,9 +5,13 @@
 * Created on: 9/4/2025
 */
 
+#include <webgpu/webgpu.h>
 #include <iostream>
 
 int main(void){
+    // Create WebGPU instance
+    // Create a descriptor
+
     std::cout << "blok." << std::endl;
     return 0;
 }
