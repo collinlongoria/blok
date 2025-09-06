a05d52bd6311143f9e3ddb06bd22bcc6031adece
Collin Longoria <collin.longoria@digipen.edu>
2025-09-05 17:30:15 -0700

    added glm submodule

--- blok/src/main.cpp
+++ blok/src/main.cpp
@@ -8,6 +8,8 @@
 #include <webgpu.h>
 #include <iostream>
 
+#include <glm.hpp>
+
 int main(void){
     // Create WebGPU instance
     // Create a descriptor
@@ -21,6 +23,12 @@ int main(void){
         return 1;
     }
     std::cout << "WGPU instance: " << instance << std::endl;
+
+    glm::vec2 a(1,1);
+    glm::vec2 b(2,2);
+
+    auto c = a + b;
+    std::cout << "(" << c.x << ", " << c.y << ")" << std::endl;
 
     // Cleanup
     wgpuInstanceRelease(instance);
