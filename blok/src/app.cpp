3b8ed15a7fb8e15a758493285a19eb3dc3f736ae
Collin Longoria <collin.longoria@digipen.edu>
2025-09-16 13:23:49 -0700

    adding webgpu overrides
    note this build doesnt work due to issues with wgpu cpp header

--- blok/src/app.cpp
+++ blok/src/app.cpp
@@ -7,6 +7,12 @@
 
 #include "app.hpp"
 
+/* These includes are here so I can test build */
+#include "gpu_device.hpp"
+#include "gpu_types.hpp"
+#include "gpu_flags.hpp"
+#include "gpu_handles.hpp"
+
 using namespace blok;
 
 void App::run() {
