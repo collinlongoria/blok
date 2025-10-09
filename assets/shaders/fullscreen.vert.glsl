521a040c86db94e9ef63072fb3daace158168efb
Collin Longoria <collin.longoria@digipen.edu>
2025-10-09 12:56:58 -0700

    vulkan running now

--- assets/shaders/fullscreen.vert.glsl
+++ assets/shaders/fullscreen.vert.glsl
@@ -1,11 +1,11 @@
 #version 450
 
-layout(location=0) in vec2 in_pos;
-layout(location=1) in vec2 in_uv;
+layout(location = 0) in vec2 inPos;
+layout(location = 1) in vec2 inUV;
 
-layout(location = 0) out vec2 uv;
+layout(location = 0) out vec2 vUV;
 
-void main(){
-    uv = in_uv;
-    gl_Position = vec4(in_pos, 0.0, 1.0);
+void main() {
+    vUV = inUV;
+    gl_Position = vec4(inPos, 0.0, 1.0);
 }
