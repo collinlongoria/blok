a26df37288f6521e298e70e7b276c61181a8b193
Collin Longoria <collin.longoria@digipen.edu>
2025-12-10 13:23:27 -0800

    starting material implimentation

---
+++ blok/include/material.hpp
@@ -0,0 +1,11 @@
+/*
+* File: ${FILE}.${EXTENSION}
+* Project: ${PROJECT}
+* Author: Collin
+* Created on: 12/10/2025
+*/
+
+#ifndef BLOK_MATERIAL_HPP
+#define BLOK_MATERIAL_HPP
+
+#endif //BLOK_MATERIAL_HPP
