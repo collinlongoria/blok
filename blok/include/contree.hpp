cbfd8fbdb4e6b17679153ff1e5d6de7831dedff7
Collin Longoria <collin.longoria@digipen.edu>
2025-12-13 11:56:38 -0800

    contree implementation

---
+++ blok/include/contree.hpp
@@ -0,0 +1,11 @@
+/*
+* File: ${FILE}.${EXTENSION}
+* Project: ${PROJECT}
+* Author: Collin
+* Created on: 12/13/2025
+*/
+
+#ifndef BLOK_CONTREE_HPP
+#define BLOK_CONTREE_HPP
+
+#endif //BLOK_CONTREE_HPP
---
+++ blok/src/contree.cpp
@@ -0,0 +1,6 @@
+/*
+* File: ${FILE}.${EXTENSION}
+* Project: ${PROJECT}
+* Author: Collin
+* Created on: 12/13/2025
+*/
